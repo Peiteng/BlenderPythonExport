141636b026cf80e84dc9b9125325c705ca5f51d8
MSIDesktop <spt0501@163.com>
2022-02-26 22:54:42 +0800

    update exporting path info

--- export_single_path_info.py
+++ export_single_path_info.py
@@ -12,6 +12,14 @@ print(begin_point)
 begin_pos=[path_pos[x]+begin_point[x] for x in range(3)]
 end_pos=[path_pos[x]+end_point[x] for x in range(3)]
 
+single_path_info={};
+single_path_info["name"]="Sphere_0";
+single_path_info["begin_pos"]=begin_pos;
+single_path_info["end_pos"]=end_pos;
 
 print('"begin_pos":',begin_pos,',')
 print('"end_pos":',end_pos)
+print('----write to json file-----')
+import json
+with open('single_path_info.json','w') as f:
+    json.dump(single_path_info, f,indent=4,sort_keys=True)
